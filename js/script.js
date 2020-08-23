40cca4f895beec09931ddbaa6329594219ad5ef2
calfuzion <51188209+calfuzion@users.noreply.github.com>
2020-08-24 06:21:19 +1200

    Magic 8 Ball
    No more bingo for now we have a working consept that will be build apon for a magic 8 ball

--- js/script.js
+++ js/script.js
@@ -1,47 +1,23 @@
-var bingo = ["I'm on the dole", "Got a smoke", "I'm banned from ... Venue", "want a smoke", "By us a jug", "want a top up", "I get paid on tuesdays", "can i have some of that", "I live at home", "I'm off guys", "don't be a dick", "Shots", "I feel safe at The horn", "Its the only bar i drink at", "The he is the legend", "Tiger Tower please"];
-var replaceData = document.querySelectorAll('table tr td');
-// console.log(bingo.length);
-
-// bingo.forEach(function(item, index, array){
-// 	// console.log(item, index);
-// 	console.log(Math.random(index, item));
-// });
-function shuffle(bingo){
-	// var currentValue = bingo.length, newValue, newIndex;
-	
-	// while(0 !== currentValue){
-	// 	randomIndex = Math.floor(Math.random()* currentValue);
-	// 	currentValue -= 1;
-	// 	newValue = array[currentValue];
-	// 	array[currentValue] = array[randomIndex];
-	// 	array[randomIndex] = newValue;
-var currentIndex = bingo.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-  var  temporaryValue = bingo[currentIndex];
-    bingo[currentIndex] = bingo[randomIndex];
-    bingo[randomIndex] = temporaryValue;
-
+// Defining Responses
+var responses = ["Your Future doesn't look to bright", "The Sky gods say its possible", "Try again Later", "Meow Meow... I'm a cat", "Yes I can confirm", "Are you really Taking advice from a magic 8 ball", "Your question is important to us please take a number and we will be with you shortly","Sorry your shit out of luck", "You're not alone on this one", "Fuck it go for it whats the worst that could happen"];
+// Generating a random response from the array
+function randomize(responses){
+	var originalIndex = responses.length, NewIndex, randomIndex;
+	// For number in array with out a randomised new index value 
+	while(0 !== originalIndex){
+		// Calcuates a new index number for item in array
+		randomIndex = Math.floor(Math.random() * originalIndex);
+		// Removes 1 value from responses.length
+		originalIndex -= 1;
+		var tempValue = responses[originalIndex];
+		responses[originalIndex] = responses[randomIndex];
+		responses[randomIndex] = tempValue;
 	}
-	return bingo;
+	// Returns new array with  new indexes;
+	return responses
+	
 };
+randomize(responses);
 
-shuffle(bingo);
-// console.log(bingo);
-// console.log(replaceData.length);
-// replaceData.innerHTML = bingo[0];
-
-// bingo.forEach(function(item){
-// 	replaceData.innerHTML = bingo;
-// });
-for(let i = 0; i < replaceData.length; ++i){
-	replaceData[i].innerHTML = bingo[i];
-
-};
+// appending response to html;
+document.getElementById("output").innerHTML = responses[1];
